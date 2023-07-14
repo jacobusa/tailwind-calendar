5c3300f03080b3b4d00384bb53c16dd1b25ab126
Kobus <jacobusbad@gmail.com>
2023-07-14 08:06:35 -0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { SimpleSchedule } from "./components/SimpleSchedule";
-import { MonthlySchedule } from "./components/MonthlySchedule";
 
 export const App: React.FC = () => {
   return (
